30b6c1381b90af6231975200a0020db262542683
methusalah <ben.oeilnoir@gmail.com>
2015-02-05 17:38:12 +0100

    rien d'interessant

---
+++ burnemallEclipseProject/src/Wave.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Wave {
+
+	List<SootBall> sootballs = new ArrayList<>();
+	List<SootBall> sootballs = new ArrayList<>();
+	
+	
+}
--- burnemallEclipseProject/src/beam/util/Prop.java
+++ burnemallEclipseProject/src/beam/util/Prop.java
@@ -2,7 +2,7 @@ package beam.util;
 
 public class Prop {
 
-	public static int PANEL_WIDTH = 400;
-	public static int PANEL_HEIGHT = 300;
+	public static int PANEL_WIDTH = 800;
+	public static int PANEL_HEIGHT = 600;
 	
 }
